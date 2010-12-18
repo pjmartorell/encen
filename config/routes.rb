ce2a3bc1f2a69d7e6f4ea65e2d942ddd2c1e1bca
Roger Campos <roger@itnig.net>
2010-12-18 13:26:29 +0100

    basic scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 BasicApp::Application.routes.draw do
   devise_for :users
 
+  match 'contact', :to => "static#contact"
+  match 'gallery', :to => "static#gallery"
+
   root :to => "static#index"
 end
