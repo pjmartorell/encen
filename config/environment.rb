6023e3094ffe194fdb23e6af470734adc26e948d
Roger Campos <roger@itnig.net>
2010-12-24 10:13:15 +0100

    adding support for getting app_config from env variables (heroku)

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/app.yml")[Rails.env]
-
 # Initialize the rails application
 Encen::Application.initialize!
