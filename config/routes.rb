4711ba9d9d577643854ef5c8c648b1a1b0aa4b50
Roger Campos <roger@itnig.net>
2011-02-09 19:24:55 +0100

    Adding locale handling from path prefix

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Encen::Application.routes.draw do
-  devise_for :users, :controllers => {:sessions => 'user_sessions'}
+  scope "(:locale)" do
+    devise_for :users, :controllers => {:sessions => 'user_sessions'}
 
-  match 'contact', :to => "static#contact"
-  match 'gallery', :to => "static#gallery"
+    match 'contact', :to => "static#contact"
+    match 'gallery', :to => "static#gallery"
 
-  namespace :admin do
-    resources :users, :except => :index
+    namespace :admin do
+      resources :users, :except => :index
 
-    root :to => "users#index"
+      root :to => "users#index"
+    end
+
+    root :to => "static#index"
   end
-
-  #match 'admin', :to => "interpret/translations#index"
-  root :to => "static#index"
 end
