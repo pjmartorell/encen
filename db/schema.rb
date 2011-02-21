bb48687cfe17534268c2c7ba5496da1689a5abdd
Roger Campos <roger@itnig.net>
2011-02-21 18:42:41 +0100

    scheme load

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101222154316) do
+ActiveRecord::Schema.define(:version => 20110221172130) do
 
   create_table "translations", :force => true do |t|
     t.string  "locale"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20101222154316) do
     t.text    "value"
     t.text    "interpolations"
     t.boolean "is_proc",        :default => false
+    t.boolean "protected",      :default => false
   end
 
   create_table "users", :force => true do |t|
