32598ad3ef958193b4d04f05981ca44bda309bb2
Bernat Farrero <bernat@itnig.net>
2011-03-14 17:15:03 +0100

    changing name links

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Encen::Application.routes.draw do
   devise_for :users, :controllers => {:sessions => 'user_sessions'}
 
-  match 'contact', :to => "static#contact"
-  match 'gallery', :to => "static#gallery"
-  match 'particular', :to => "static#particular"
+  match 'contacto', :to => "static#contacto"
+  match 'servicios', :to => "static#servicios"
   match 'empresas', :to => "static#empresas"
-  match 'finques', :to => "static#finques"
-  match 'seguros', :to => "static#seguros"
+  match 'noticias', :to => "static#noticias"
 
   match 'admin', :to => "admin/translations#index"
   root :to => "static#index"
