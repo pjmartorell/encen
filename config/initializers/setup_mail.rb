6bba002f3afac71fb66068d0d1b338f88e390658
Roger Campos <roger@itnig.net>
2011-03-15 12:45:21 +0100

    Fix mailer setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,9 +2,9 @@ ActionMailer::Base.smtp_settings = {
   :address              => APP_CONFIG['mail']['smtp']['address'],
   :port                 => APP_CONFIG['mail']['smtp']['port'],
 
-  :domain               => APP_CONFIG['mail']['domain'],
-  :user_name            => APP_CONFIG['mail']['user_name'],
-  :password             => APP_CONFIG['mail']['password'],
+  :domain               => APP_CONFIG['mail']['smtp']['domain'],
+  :user_name            => APP_CONFIG['mail']['account']['user_name'],
+  :password             => APP_CONFIG['mail']['account']['password'],
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
