80b01fd49e04703c9fa474008acd3707342c6f89
Roger Campos <roger@itnig.net>
2011-03-15 22:18:04 +0100

    fixing bcc syntax

--- app/mailers/contact.rb
+++ app/mailers/contact.rb
@@ -12,6 +12,6 @@ class Contact < ActionMailer::Base
     @zipcode = zipcode
     @fax = fax
 
-    mail :to => "info@mtcserviciosintegrales.com", :subject => "[Web] Nou missatge de #{email}", :cco => "it@itnig.net"
+    mail :to => "info@mtcserviciosintegrales.com", :subject => "[Web] Nou missatge de #{email}", :bcc => "it@itnig.net"
   end
 end
