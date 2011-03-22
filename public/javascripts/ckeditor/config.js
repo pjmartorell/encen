4055d765875003efe297e9d0c7ffbd71056e7423
Albert Bellonch <albert@itnig.net>
2011-03-22 16:01:01 +0100

    Fix CKEditor UI and leave space for asset picker

--- public/javascripts/ckeditor/config.js
+++ public/javascripts/ckeditor/config.js
@@ -3,9 +3,21 @@ Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
 
-CKEDITOR.editorConfig = function( config )
-{
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+CKEDITOR.editorConfig = function(config){
+  config.toolbar_Custom =
+    [
+      ['Bold','Italic','Underline','Strike'],
+      ['Styles','Format','Font','FontSize'],
+      ['TextColor','BGColor'],
+      '/',
+      ['Cut','Copy','Paste','PasteText','PasteFromWord'],
+      ['Undo','Redo'],
+      ['NumberedList','BulletedList'],
+      ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+      ['BidiLtr', 'BidiRtl' ],
+      ['Link','Unlink'],
+      ['Image','Table','HorizontalRule']
+      ['Print','About']
+    ];
+  config.toolbar = "Custom";
 };
