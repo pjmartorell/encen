4b897bca08001d07f6fe250e24a2d2fded97dff5
Albert Bellonch <albert@itnig.net>
2011-03-23 12:10:02 +0100

    Destroy comments and images of a post when destroying it

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
-  has_many :images
+  has_many :comments, :dependent => :destroy
+  has_many :images, :dependent => :destroy
 
   validates_presence_of :title
   validates_presence_of :body, :on => :update
