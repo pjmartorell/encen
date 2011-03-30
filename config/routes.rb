de46703566410a0d8b4e2c53812696e45992a884
Dulce De La Rosa <dulce@itnig.net>
2011-03-30 16:53:19 +0200

    adding static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Encen::Application.routes.draw do
-  
+
 
   scope "(:locale)" do
     devise_for :users, :controllers => {:sessions => 'user_sessions'}
 
-    match 'contact', :to => "static#contact"
+    match 'contacto', :to => "static#contact"
+    match 'premama', :to => "static#pregnant"
+    match 'ropa0_3', :to => "static#baby-clothes0-3"
+    match 'ropa4_8', :to => "static#baby-clothes4-8"
+    match 'cuidados', :to => "static#cares"
+    match 'tienda', :to => "static#shop"
+    match 'complementos', :to => "static#complements"
     match 'gallery', :to => "static#gallery"
 
     namespace :admin do
