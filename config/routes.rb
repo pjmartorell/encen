b11038239729ab8d709ce678c0fa4feeb3db8407
Dulce De La Rosa <dulce@itnig.net>
2011-04-06 18:37:32 +0200

    adding content and subpages

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Encen::Application.routes.draw do
 
     match 'especialidades', :to => "static#areas_of_practice"
     match 'consulta', :to => "static#query"
+    match 'civil', :to => "static#civil"
+    match 'penal', :to => "static#criminal"
+    match 'familiar', :to => "static#family"
+    match 'sucesions', :to => "static#sucesions"
     match 'contacto', :to => "static#contact"
 
     namespace :admin do
