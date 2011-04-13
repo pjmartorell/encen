b2536d90b94b2b994b476e5c0e7837767ae6babc
Roger Campos <roger@itnig.net>
2011-04-13 19:28:00 +0200

    editable pages from backoffice

--- app/controllers/admin/page_contents_controller.rb
+++ app/controllers/admin/page_contents_controller.rb
@@ -5,7 +5,14 @@ class Admin::PageContentsController < Admin::AdminController
   end
 
   def update
+    @page_content = PageContent.find params[:id]
+    @page = @page_content.page
 
+    if @page_content.update_attributes params[:page_content]
+      redirect_to admin_pages_url
+    else
+      render :action => :edit
+    end
   end
 end
 
