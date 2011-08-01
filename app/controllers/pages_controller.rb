3ca44b05fafbcc69c2ae20b31a01f8c8fc76a507
Pere Joan Martorell <perejoan@itnig.net>
2011-08-02 01:38:18 +0200

    Canvis menors

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def show
     @page_content = PageContent.find_by_url params[:page]
     unless @page_content
-      raise ActionController::RoutingError, "invalid route"
+      raise ActionController::RoutingError, "Pagina no trobada"
       return
     end
     @page = @page_content.page
