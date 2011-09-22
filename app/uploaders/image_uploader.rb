230361c633e202fdab8eb9acdb324cdcdfde1a23
Pere Joan Martorell <perejoan@itnig.net>
2011-09-22 23:05:17 +0200

    8e intent?

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,11 +8,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "https://s3.amazonaws.com/romeu/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
   def cache_dir
-    "https://s3.amazonaws.com/romeu/uploads/cache"
+    "uploads/cache"
   end
   
   # Album Cover version
