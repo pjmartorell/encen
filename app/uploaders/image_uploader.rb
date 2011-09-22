63753a3d357098db153d87113f3342019f226534
Pere Joan Martorell <perejoan@itnig.net>
2011-09-22 20:14:30 +0200

    5e intent

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,6 +11,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
   
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
+  
   # Album Cover version
   version :cover do
     process :square_resize => [150,150]
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp')
-  config.cache_dir =  Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
   config.fog_credentials = {
     :provider               => 'AWS', 
     :aws_access_key_id      => APP_CONFIG['storage']['s3_access'],
