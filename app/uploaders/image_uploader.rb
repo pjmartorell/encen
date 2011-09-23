586278e51ff2a2d53ab1c75dca587f3b02fdc7ba
Pere Joan Martorell <perejoan@itnig.net>
2011-09-23 21:25:46 +0200

    Provant canviar sintaxi

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -8,7 +8,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   storage :fog
 
   def store_dir
-    "uploads/#{ model.class.to_s.underscore }/#{ mounted_as }/#{ model.id }"
+    "uploads"
   end
   
   # Album Cover version
