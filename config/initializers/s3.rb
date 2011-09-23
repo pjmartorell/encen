d526823170dea9404cce234c941ccfb57928b9ce
Pere Joan Martorell <perejoan@itnig.net>
2011-09-23 13:23:17 +0200

    Continuant arreglar problemes Carrierwave 6

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -9,8 +9,8 @@ CarrierWave.configure do |config|
     config.fog_host       = 'http://romeu.s3.amazonaws.com'
     config.fog_public     = true
     config.storage        = :fog
-    config.root = Rails.root.join('tmp') # adding these...
-    config.cache_dir = 'uploads' # ...two lines
+    config.root = Rails.root.join('tmp') 
+    config.cache_dir = 'uploads' 
   else
     config.storage        = :file
   end
