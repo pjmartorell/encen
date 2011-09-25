ace9beca1643db070a49680e73a4befc00efd19c
Pere Joan Martorell <perejoan@itnig.net>
2011-09-25 18:02:31 +0200

    Canvis en credencials S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,7 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS', 
       :aws_access_key_id      => ENV['s3_access_key_id'],
-      :aws_secret_access_key  => ENV['s3_secret_access_key'],
-      :region                 => 'eu-west-1'
+      :aws_secret_access_key  => ENV['s3_secret_access_key']
     }
     config.fog_directory  = 'romeu'
     config.fog_host       = 'http://romeu.s3.amazonaws.com'
