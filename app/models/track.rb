f2d611c406c715b206deff86e38668f20f2b1d3b
Pere Joan Martorell <perejoan@itnig.net>
2011-10-09 16:55:35 +0200

    Afegint default scope per tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -3,5 +3,6 @@ class Track < ActiveRecord::Base
   has_one :document, :as => :owner, :dependent => :destroy
   mount_uploader :doc, DocumentUploader
   
+  default_scope order('created_at asc')
   validates_presence_of :name  
 end
