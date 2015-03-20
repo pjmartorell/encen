c2262fc4fed2358c0e3f520b9e89bdc13470f8aa
Pere Joan Martorell <perejoan@itnig.net>
2015-03-20 14:00:35 +0100

    Fixing Carrierwave config option after updating gems

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,18 +1,18 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
-      :provider               => 'AWS', 
+      :provider               => 'AWS',
       :aws_access_key_id      => APP_CONFIG['storage']['s3_access'],
       :aws_secret_access_key  => APP_CONFIG['storage']['s3_secret']
     }
     config.fog_directory  = 'romeu'
-    config.fog_host       = 'http://romeu.s3.amazonaws.com'
+    config.asset_host       = 'http://romeu.s3.amazonaws.com'
     config.fog_public     = true
-    
+
     config.root           = Rails.root.join('tmp')
     config.cache_dir      = 'carrierwave'
     config.storage        = :fog
-    
+
   else
     config.storage        = :file
   end
